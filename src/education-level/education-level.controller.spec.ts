d3d48efd223f2bf71c101aff5b8c8dbdde409bac
jeffyso <jeffyzaja@gmail.com>
2021-02-09 18:22:25 +0700

    EDIT-education-level.spec

--- src/education-level/education-level.controller.spec.ts
+++ src/education-level/education-level.controller.spec.ts
@@ -1,20 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseModule } from '../database/database.module';
+import { CreateEducationLevelDto } from './dto/create-education-level.dto';
 import { EducationLevelController } from './education-level.controller';
 import { EducationLevelService } from './education-level.service';
+import { educationLevelProviders } from './providers/education-level.provider';
 
-describe('EducationLevelController', () => {
+
+describe.only('EducationLevelController', () => {
   let controller: EducationLevelController;
+  let db: EducationLevelController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [EducationLevelService, ...educationLevelProviders],
       controllers: [EducationLevelController],
-      providers: [EducationLevelService],
+      imports: [
+        DatabaseModule,
+      ],
     }).compile();
 
     controller = module.get<EducationLevelController>(EducationLevelController);
+    db = module.get<EducationLevelController>(EducationLevelController);
   });
 
-  it('should be defined', () => {
+  afterEach(async () => {
+    const response = await db.remove.name
+    expect(response)
+    console.log(response)
+  })
+
+  it('should be defined', async () => {
+
+    const a = new CreateEducationLevelDto();
+    a.name = "sddsads"
+
+    const response = await controller.create(a)
     expect(controller).toBeDefined();
+
+    delete response.data.id
+    expect(response).toEqual({
+      data: {
+        name: "sddsads"
+      }
+    })
   });
 });
