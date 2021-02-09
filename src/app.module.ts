84e9dc52f67ec4a35c8fe88e9244255ffae293a0
jeffyso <jeffyzaja@gmail.com>
2021-02-09 10:22:25 +0700

    EDIT-add.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { EducationLevelModule } from './education-level/education-level.module';
 
 @Module({
-  imports: [],
+  imports: [EducationLevelModule,DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
