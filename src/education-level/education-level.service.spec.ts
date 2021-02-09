2d46e16a92ac34e9960461c6eaa2c6ea58acc56d
jeffyso <jeffyzaja@gmail.com>
2021-02-09 18:22:39 +0700

    EDIT-education-level.spec

--- src/education-level/education-level.service.spec.ts
+++ src/education-level/education-level.service.spec.ts
@@ -1,18 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { EducationLevelService } from './education-level.service';
+import { DatabaseModule } from '../database/database.module';
+import { EducationLevelController } from '../education-level/education-level.controller';
+import { EducationLevelService } from '../education-level/education-level.service';
+import { educationLevelProviders } from '../education-level/providers/education-level.provider';
 
 describe('EducationLevelService', () => {
   let service: EducationLevelService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EducationLevelService],
+      providers: [EducationLevelService, ...educationLevelProviders],
+      controllers: [EducationLevelController],
+      imports: [
+        DatabaseModule,
+      ],
     }).compile();
 
     service = module.get<EducationLevelService>(EducationLevelService);
   });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+      expect(service).toBeDefined;
+    })
 });
