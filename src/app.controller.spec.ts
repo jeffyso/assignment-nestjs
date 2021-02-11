05c225900ceccabf8ea6e2c76fbf9cbbcbb9ba45
jeffyso <jeffyzaja@gmail.com>
2021-02-11 10:32:39 +0700

    EDIT-App.controller.spec

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-describe.skip('AppController', () => {
+describe('AppController', () => {
   let appController: AppController;
   let module: TestingModule;
   beforeEach(async () => {
