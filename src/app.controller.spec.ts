60217f2cb669a7d45608e0efa7ff55b676f7714f
jeffyso <jeffyzaja@gmail.com>
2021-02-11 11:14:13 +0700

    EDIT-app.controller.spec

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,17 @@
+import { NestFactory } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import { AppModule } from './app.module';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { EducationLevelModule } from './education-level/education-level.module';
 
 describe('AppController', () => {
   let appController: AppController;
   let module: TestingModule;
   beforeEach(async () => {
     module = await Test.createTestingModule({
+      imports: [EducationLevelModule,DatabaseModule,AppModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -26,3 +31,4 @@ describe('AppController', () => {
     });
   });
 });
+
